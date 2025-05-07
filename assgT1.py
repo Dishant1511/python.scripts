ae90d8e97b774660465ff80135d192cbeaf0d242
Dishant1511 <dishantjarwal2002@gmail.com>
2025-05-08 00:41:52 +0530

    Add files via upload

---
+++ assgT1.py
@@ -0,0 +1,20 @@
+#Tassk 1
+'''
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+a1 = a+b
+a2 = a-b
+a3 = a*b
+a4 = a//b
+print("Addition : ",a1)
+print("Subtraction : ",a2)
+print("Multiplication : ",a3)
+print("Division : ",a4)
+'''
+
+#Task 2
+
+a = input("Enter the first name: ")
+b = input("Enter the last name: ")
+c = a+" " +b
+print("Hello",c ,"! Welcome to the Python Program")
